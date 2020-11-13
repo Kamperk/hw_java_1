8c31fb8801120f2ad19d61800dc877fe24c8e0c6
Masson070707 <kapustin-19-08@mail.ru>
2020-11-13 19:17:36 +0300

    change_type_2

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         int balance = 2_000_000_000;
         int payment = 500_000_000;
-        long total = balance + payment;
+        int total = balance + payment;
         System.out.println(total);
 
     }
